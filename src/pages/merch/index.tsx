63dd9693de19c9e60b35913974f1e3804e4107b0
mattdegods <matthewcpfeifer@gmail.com>
2023-07-24 15:30:04 -0400

    Fix: /merch was empty

---
+++ src/pages/merch/index.tsx
@@ -0,0 +1,12 @@
+import { Nav } from "@/components";
+import { FC } from "react";
+
+const Merch: FC = () => {
+  return (
+    <div className="w-full min-h-screen flex flex-col items-center">
+      <Nav />
+    </div>
+  );
+};
+
+export default Merch;
