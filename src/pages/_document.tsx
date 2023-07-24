248b09c4ebca4e19da1b24b070236279e79c1291
mattdegods <matthewcpfeifer@gmail.com>
2023-07-24 16:16:47 -0400

    Fix: meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,9 @@
-import { PageHead } from "@/components";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <PageHead
-        title="Mickey DeGods"
-        description="The original DeGods sub-community."
-        url="https://mickeydegods.com/"
-        twitter="MickeyDeGods"
-      />
+      <Head />
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,18 @@ import {
   MenuView,
   CollabsView,
   WalkInCooler,
+  PageHead,
 } from "@/components";
 
 export default function Home() {
   return (
     <div className="w-full min-h-screen flex flex-col items-center">
+      <PageHead
+        title="Mickey DeGods"
+        description="The original DeGods sub-community."
+        url="https://mickeydegods.com/"
+        twitter="MickeyDeGods"
+      />
       <LandingView />
       <AboutView />
       <MembersView />
