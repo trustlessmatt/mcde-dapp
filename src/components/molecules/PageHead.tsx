525667205e99456e6b6b11970695b01ae5d18041
mattdegods <matthewcpfeifer@gmail.com>
2023-07-24 16:12:18 -0400

    Fix: meta tag

--- src/components/molecules/PageHead.tsx
+++ src/components/molecules/PageHead.tsx
@@ -18,37 +18,17 @@ const PageHead: FC<Props> = (props: Props) => {
       <link rel="icon" href="/favicon.ico" />
       {/* twitter */}
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@allintime_" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content="https://allintime.xyz/meta.png" />
-      <meta property="twitter:url" content={`https://allintime.xyz/`} />
-      {/* <!-- Open Graph / Facebook --> */}
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://allintime.xyz/" />
-      <meta property="og:title" content="All in Time" />
-      <meta
-        property="og:description"
-        content="clothing | collectibles | curio | Est. 2023"
-      />
-      <meta property="og:image" content="/meta.png" />
-
-      {/* <title>{title}</title>
-      <meta name="description" content={description} />
-      <link rel="icon" href="/favicon.ico" /> */}
-      {/* twitter */}
-      {/* <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content={`@${twitter}`} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={`${url}meta.png`} />
-      <meta property="twitter:url" content={url} /> */}
+      <meta property="twitter:url" content={url} />
       {/* <!-- Open Graph / Facebook --> */}
-      {/* <meta property="og:type" content="website" />
+      <meta property="og:type" content="website" />
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={`/meta.png`} /> */}
+      <meta property="og:image" content={`/meta.png`} />
     </Head>
   );
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,14 +4,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <PageHead
-          title="Mickey DeGods"
-          description="The original DeGods sub-community."
-          url="https://mickeydegods.com/"
-          twitter="MickeyDeGods"
-        />
-      </Head>
+      <PageHead
+        title="Mickey DeGods"
+        description="The original DeGods sub-community."
+        url="https://mickeydegods.com/"
+        twitter="MickeyDeGods"
+      />
       <body>
         <Main />
         <NextScript />
