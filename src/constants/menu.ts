61cc4342d610fe742130236c449a9233b29b4ffb
mattdegods <matthewcpfeifer@gmail.com>
2023-07-29 16:44:17 -0400

    Fix: Update menu item counts

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -4,27 +4,27 @@ export const menu: MenuItem[] = [
   {
     name: "Fries",
     image: `${process.env.NEXT_PUBLIC_CDN_URL}/images/menu/fries.png`,
-    description: "25 pc",
+    description: "40 pc",
   },
   {
     name: "Burgers",
     image: `${process.env.NEXT_PUBLIC_CDN_URL}/images/menu/burger.png`,
-    description: "100 pc",
+    description: "75 pc",
   },
   {
     name: "Nuggets",
     image: `${process.env.NEXT_PUBLIC_CDN_URL}/images/menu/nuggets.png`,
-    description: "100 pc",
+    description: "75 pc",
   },
   {
     name: "Drinks",
     image: `${process.env.NEXT_PUBLIC_CDN_URL}/images/menu/drink.png`,
-    description: "100 pc",
+    description: "75 pc",
   },
   {
     name: "Ice Cream",
     image: `${process.env.NEXT_PUBLIC_CDN_URL}/images/menu/ice_cream.png`,
-    description: "100 pc",
+    description: "75 pc",
   },
   {
     name: "Special Fries",
