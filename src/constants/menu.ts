b64713db1d0a3058eb2bf0e64d1754343fd356d9
mattdegods <matthewcpfeifer@gmail.com>
2023-08-15 12:44:16 -0400

    Fix: menu item collection sizes

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -2,29 +2,29 @@ import { MenuItem } from "@/types";
 
 export const menu: MenuItem[] = [
   {
-    name: "Fries",
+    name: "Classic Fries",
     image: `${process.env.NEXT_PUBLIC_CDN_URL}/images/menu/fries.png`,
-    description: "40 pc",
+    description: "20 pc",
   },
   {
     name: "Burgers",
     image: `${process.env.NEXT_PUBLIC_CDN_URL}/images/menu/burger.png`,
-    description: "75 pc",
+    description: "250 pc",
   },
   {
     name: "Nuggets",
     image: `${process.env.NEXT_PUBLIC_CDN_URL}/images/menu/nuggets.png`,
-    description: "75 pc",
+    description: "200 pc",
   },
   {
     name: "Drinks",
     image: `${process.env.NEXT_PUBLIC_CDN_URL}/images/menu/drink.png`,
-    description: "75 pc",
+    description: "150 pc",
   },
   {
     name: "Ice Cream",
     image: `${process.env.NEXT_PUBLIC_CDN_URL}/images/menu/ice_cream.png`,
-    description: "75 pc",
+    description: "69 pc",
   },
   {
     name: "Special Fries",
