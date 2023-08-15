248c1b7577e6dc26c421fb274ca21080bf7037ee
mattdegods <matthewcpfeifer@gmail.com>
2023-08-15 12:41:16 -0400

    Fix: TLDR content update

--- src/components/atoms/TLDR.tsx
+++ src/components/atoms/TLDR.tsx
@@ -4,16 +4,17 @@ const TLDR: FC = () => (
   <div className="flex flex-col w-full max-w-[462px] items-start gap-5 text-lg px-5">
     <h2>Welcome to Mickey DeGods.</h2>
     <p>
-      A group of creators, entrepreneurs, artists, experimenters, thought
-      leaders, & market makers.
+      An open group of web3 homies with a passion for trying new degen shit.
+      Includes the best creators, entrepreneurs, artists, experimenters, thought
+      leaders, & market makers DeGods has to offer.
     </p>
-    <ul className="">
-      <li className="font-primaryBold">TL;DR</li>
+    <ul>
       <li>👉 1st ever DeGods sub-community</li>
-      <li>👉 Elite holders</li>
       <li>👉 The infamous “Mickey DeGods Walk-In” holder only chat</li>
       <li>👉 Unmatched alpha, giveaways, & WL opportunities</li>
-      <li>👉 Launched a sub-collection that’s still emitting $DUST</li>
+      <li>👉 Multiple subcollections launched</li>
+      <li>👉 Collabs with top artistis</li>
+      <li>👉 Deep $DUST pockets to fund our expansion</li>
       <li>👉 We’re burnin’ it!</li>
     </ul>
   </div>
