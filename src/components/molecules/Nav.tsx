1d2a5bc208c3fbabc178f236aa374babbed9acbc
mattdegods <matthewcpfeifer@gmail.com>
2023-08-15 23:00:34 -0400

    Feat: add back staking link

--- src/components/molecules/Nav.tsx
+++ src/components/molecules/Nav.tsx
@@ -78,8 +78,14 @@ const Nav: FC = () => {
             <p
               className={`${
                 pathname === "/staking" && "font-primaryBold"
-              } cursor-not-allowed`}
-              // onClick={() => handleRouter("/staking")}
+              } cursor-pointer`}
+              onClick={() =>
+                window.open(
+                  "https://anybodies.com/c/mickeydegods",
+                  "_blank",
+                  "noopener noreferrer"
+                )
+              }
             >
               Staking
             </p>
